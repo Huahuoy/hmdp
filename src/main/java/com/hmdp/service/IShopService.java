6c9087087977e2e54d93995d3da8eac3df7dd294
Huahuoy <1554427917@qq.com>
2024-05-28 21:00:42 +0800

    :apple:地理位置商铺

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -15,4 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface IShopService extends IService<Shop> {
 
     Result getByIdWithCache(Long id);
+
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
 }
