ba5bc82494cda92911d4a83397fd6aebaf6e12cb
Huahuoy <1554427917@qq.com>
2024-06-08 16:06:11 +0800

    :sparkles:Minio实现上传探店照片

--- src/main/java/com/hmdp/controller/UploadController.java
+++ src/main/java/com/hmdp/controller/UploadController.java
@@ -1,21 +1,19 @@
 package com.hmdp.controller;
 
-import cn.hutool.core.io.FileUtil;
+
 import cn.hutool.core.lang.UUID;
 
 import com.hmdp.dto.Result;
 
 import com.hmdp.service.FileStorageService;
-import com.hmdp.utils.SystemConstants;
+
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
+
 
 @Slf4j
 @RestController
